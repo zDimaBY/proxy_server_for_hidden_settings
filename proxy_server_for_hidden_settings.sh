2e9641fb682da31d4f68be0cfeae0290df35d44a
zDimaBY <zdimaby@gmail.com>
2024-02-11 20:39:58 +0200

    v0.2

--- proxy_server_for_hidden_settings.sh
+++ proxy_server_for_hidden_settings.sh
@@ -22,7 +22,7 @@ function check_dependency() {
     else
         echo "Не вдалося визначити операційну систему."
     fi
-    
+
     if ! command -v $dependency_name &>/dev/null; then
         echo -e "${RED}$dependency_name не встановлено. Встановлюємо...${RESET}"
         if ! "$UPDATE_DONE"; then
@@ -48,7 +48,7 @@ function check_dependency() {
                 return 1
                 ;;
             esac
-            
+
             UPDATE_DONE=true
         else
             case $operating_system in
@@ -77,32 +77,37 @@ function check_dependency() {
     fi
 }
 
-UPDATE_DONE=false
+check_ssh_keys() {
+    REMOTE_ADDRESS_CHECK="$1"
+    if [ ! -f ~/.ssh/id_rsa ]; then
+        ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+    fi
+    if ssh-keygen -F "$REMOTE_ADDRESS_CHECK" -l &>/dev/null; then
+        echo "Ключі для сервера $REMOTE_ADDRESS_CHECK вже існують. Видаляємо їх..."
+        ssh-keygen -R "$REMOTE_ADDRESS_CHECK" -f ~/.ssh/known_hosts
+        ssh-keyscan -H "$REMOTE_ADDRESS_CHECK" >>~/.ssh/known_hosts
+    else
+        echo "Додаємо ключі для сервера $REMOTE_ADDRESS_CHECK..."
+        ssh-keyscan -H "$REMOTE_ADDRESS_CHECK" >>~/.ssh/known_hosts
+    fi
+    sshpass -p "$REMOTE_PASSWORD" ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no "$REMOTE_USER"@"$REMOTE_ADDRESS_CHECK"
+}
 
+UPDATE_DONE=false
+# Перевірка залежностей
 dependencies=(
     "sshpass sshpass"
-    "wireguard wireguard"
+    "wg wireguard"
     "resolvconf resolvconf"
+    "curl curl"
 )
 
 for dependency in "${dependencies[@]}"; do
     check_dependency $dependency
 done
 
-# Генерація rsa SSH ключа
-if [ ! -f ~/.ssh/id_rsa ]; then
-    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
-fi
-
-if ssh-keygen -F "$REMOTE_ADDRESS" -l &> /dev/null; then
-    echo "Ключі для сервера $REMOTE_ADDRESS вже існують. Видаляємо їх..."
-    ssh-keygen -R "$REMOTE_ADDRESS" -f ~/.ssh/known_hosts
-else
-    echo "Додаємо ключі для сервера $REMOTE_ADDRESS..."
-    ssh-keyscan -H "$REMOTE_ADDRESS" >> ~/.ssh/known_hosts
-fi
-
-sshpass -p $REMOTE_PASSWORD ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_ADDRESS
+# Перевірка наявності ключів SSH для віддаленого сервера
+check_ssh_keys $REMOTE_ADDRESS
 
 REMOTE_COMMAND="wget https://raw.githubusercontent.com/zDimaBY/setting_up_control_panels/main/scripts/5_VPN.sh && source /etc/os-release && operating_system="\$ID" && source /root/5_VPN.sh && mkdir -p /root/VPN/wireguard && install_wireguard_scriptLocal"
 sshpass -p "$REMOTE_PASSWORD" ssh -o PasswordAuthentication=no -x "$REMOTE_USER"@"$REMOTE_ADDRESS" "$REMOTE_COMMAND"
@@ -111,3 +116,10 @@ sshpass -p "$REMOTE_PASSWORD" rsync -e "ssh -o StrictHostKeyChecking=no" -avz "$
 
 cp wg0-client-proxy.conf /etc/wireguard/wg0.conf
 wg-quick up wg0
+
+check_ssh_keys "10.0.0.1"
+
+REMOTE_COMMAND_IPTABLES="iptables -t nat -A PREROUTING -p tcp ! --dport 51820 -j DNAT --to-destination 10.0.0.2 && iptables -t nat -A POSTROUTING -d 10.0.0.2 -j SNAT --to-source $REMOTE_ADDRESS"
+sshpass -p "$REMOTE_PASSWORD" ssh -o PasswordAuthentication=no -x "$REMOTE_USER"@10.0.0.1 "$REMOTE_COMMAND_IPTABLES"
+
+echo -e "\e[32m$(curl ifconfig.me)\e[0m"
