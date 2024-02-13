da5b94d726d8e3eb9aa613ff92852e64a93861e9
zDimaBY <zdimaby@gmail.com>
2024-02-13 23:53:44 +0200

    v0.3

--- proxy_server_for_hidden_settings.sh
+++ proxy_server_for_hidden_settings.sh
@@ -23,9 +23,13 @@ function check_dependency() {
         echo "Не вдалося визначити операційну систему."
     fi
 
+    # Перевірка наявності залежності
     if ! command -v $dependency_name &>/dev/null; then
         echo -e "${RED}$dependency_name не встановлено. Встановлюємо...${RESET}"
+
+        # Перевірка чи вже було виконано оновлення системи
         if ! "$UPDATE_DONE"; then
+            # Встановлення залежності залежно від операційної системи
             case $operating_system in
             debian | ubuntu)
                 apt-get update
@@ -79,47 +83,91 @@ function check_dependency() {
 
 check_ssh_keys() {
     REMOTE_ADDRESS_CHECK="$1"
+    # Генерація rsa SSH ключа
     if [ ! -f ~/.ssh/id_rsa ]; then
         ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
     fi
     if ssh-keygen -F "$REMOTE_ADDRESS_CHECK" -l &>/dev/null; then
         echo "Ключі для сервера $REMOTE_ADDRESS_CHECK вже існують. Видаляємо їх..."
-        ssh-keygen -R "$REMOTE_ADDRESS_CHECK" -f ~/.ssh/known_hosts
+        ssh-keygen -R "$REMOTE_ADDRESS_CHECK" -f ~/.ssh/known_hosts # Видалення старих ключів
         ssh-keyscan -H "$REMOTE_ADDRESS_CHECK" >>~/.ssh/known_hosts
     else
         echo "Додаємо ключі для сервера $REMOTE_ADDRESS_CHECK..."
-        ssh-keyscan -H "$REMOTE_ADDRESS_CHECK" >>~/.ssh/known_hosts
+        ssh-keyscan -H "$REMOTE_ADDRESS_CHECK" >>~/.ssh/known_hosts # Додавання нових ключів
     fi
-    sshpass -p "$REMOTE_PASSWORD" ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no "$REMOTE_USER"@"$REMOTE_ADDRESS_CHECK"
+    if sshpass -p "$REMOTE_PASSWORD" ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no "$REMOTE_USER"@"$REMOTE_ADDRESS_CHECK"; then
+        echo "Ключі успішно скопійовано на віддалений сервер."
+    else
+        echo "Сталася помилка під час копіювання ключів на віддалений сервер. Спробуємо виправити."
+        if sshpass -p "$REMOTE_PASSWORD" ssh-copy-id -f -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no "$REMOTE_USER"@"$REMOTE_ADDRESS_CHECK"; then
+            echo "Ключі успішно скопійовано на віддалений сервер із використанням опції -f."
+        else
+            echo "Сталася помилка під час копіювання ключів на віддалений сервер із використанням опції -f."
+        fi
+    fi
+}
+vpnStartAndConect() {
+    if output=$(wg-quick down wg0 2>&1); then
+        if echo "$output" | grep -q "is not a WireGuard interface"; then
+            echo "Помилка: wg0 не є інтерфейсом WireGuard."
+        else
+            echo "З'єднання WireGuard wg0 розірвано."
+        fi
+    else
+        echo "Сталася помилка при виконанні команди wg-quick down wg0."
+    fi
+    while true; do
+        ping -c 1 $REMOTE_ADDRESS >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            break # Вихід з цикла.
+        else
+            sleep 60
+            echo "Сервер $REMOTE_ADDRESS не пінгується"
+        fi
+    done
+    check_ssh_keys $REMOTE_ADDRESS
+
+    REMOTE_COMMAND="wget https://raw.githubusercontent.com/zDimaBY/setting_up_control_panels/main/scripts/5_VPN.sh && \
+        source /etc/os-release && \
+        operating_system=\"\$ID\" && \
+        source /root/5_VPN.sh && \
+        mkdir -p /root/VPN/wireguard && \
+        install_wireguard_scriptLocal"
+
+    sshpass -p "$REMOTE_PASSWORD" ssh -o PasswordAuthentication=no -x "$REMOTE_USER"@"$REMOTE_ADDRESS" "$REMOTE_COMMAND"
+
+    sshpass -p "$REMOTE_PASSWORD" rsync -e "ssh -o StrictHostKeyChecking=no" -avz "$REMOTE_USER"@"$REMOTE_ADDRESS":/root/VPN/wireguard/* /root
+
+    cp /root/wg0-client-proxy.conf /etc/wireguard/wg0.conf && wg-quick up wg0
+
+    check_ssh_keys "10.0.0.1"
+
+    #REMOTE_COMMAND_IPTABLES="iptables -t nat -A PREROUTING -p tcp ! --dport 51820 -j DNAT --to-destination 10.0.0.2 && \
+    #iptables -t nat -A POSTROUTING -d 10.0.0.2 -j SNAT --to-source $REMOTE_ADDRESS"
+
+    #sshpass -p "$REMOTE_PASSWORD" ssh -o PasswordAuthentication=no -x "$REMOTE_USER"@10.0.0.1 "$REMOTE_COMMAND_IPTABLES"
 }
 
 UPDATE_DONE=false
-# Перевірка залежностей
 dependencies=(
     "sshpass sshpass"
     "wg wireguard"
     "resolvconf resolvconf"
     "curl curl"
 )
-
 for dependency in "${dependencies[@]}"; do
     check_dependency $dependency
 done
 
-# Перевірка наявності ключів SSH для віддаленого сервера
-check_ssh_keys $REMOTE_ADDRESS
+vpnStartAndConect # Виконуємо налаштуваня на віддаленому сервері.
 
-REMOTE_COMMAND="wget https://raw.githubusercontent.com/zDimaBY/setting_up_control_panels/main/scripts/5_VPN.sh && source /etc/os-release && operating_system="\$ID" && source /root/5_VPN.sh && mkdir -p /root/VPN/wireguard && install_wireguard_scriptLocal"
-sshpass -p "$REMOTE_PASSWORD" ssh -o PasswordAuthentication=no -x "$REMOTE_USER"@"$REMOTE_ADDRESS" "$REMOTE_COMMAND"
-
-sshpass -p "$REMOTE_PASSWORD" rsync -e "ssh -o StrictHostKeyChecking=no" -avz "$REMOTE_USER"@"$REMOTE_ADDRESS":/root/VPN/wireguard/* /root
-
-cp wg0-client-proxy.conf /etc/wireguard/wg0.conf
-wg-quick up wg0
-
-check_ssh_keys "10.0.0.1"
-
-REMOTE_COMMAND_IPTABLES="iptables -t nat -A PREROUTING -p tcp ! --dport 51820 -j DNAT --to-destination 10.0.0.2 && iptables -t nat -A POSTROUTING -d 10.0.0.2 -j SNAT --to-source $REMOTE_ADDRESS"
-sshpass -p "$REMOTE_PASSWORD" ssh -o PasswordAuthentication=no -x "$REMOTE_USER"@10.0.0.1 "$REMOTE_COMMAND_IPTABLES"
-
-echo -e "\e[32m$(curl ifconfig.me)\e[0m"
+while true; do
+    ping -c 1 10.0.0.1 >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        sleep 10
+    else
+        echo "Сервер 10.0.0.1 не пінгується"
+        vpnStartAndConect
+        break # Вихід з цикла.
+    fi
+done
